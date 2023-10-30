8933ae0fe69b05eeb170221a21447b3dbb64714d
juan sacco <juandsacco@gmail.com>
2023-10-30 11:17:28 -0300

    agergo modif

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
             <h4>About Me</h4>
             <p>Desde que comencé mi experiencia como desarrollador hace poco más de dos años, 
                 he realizado trabajos de manera freelance, he colaborado con personas talentosas 
-                para crear proyectos de consumo y clones como Mercado Libre siendo el mayor proyecto.
+                para crear proyectos de consumo y clones de diferentes webs.
                 Tengo una confianza silenciosa, curiosidad natural y 
                 trabajo constantemente para mejorar mis habilidades como desarrollador.</p>
             </div>
